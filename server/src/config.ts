3e6232d92cadaa5e21b0ef2e3f515a0dab1be30e
ellisaveta <elina123456@mail.bg>
2024-06-30 13:57:13 +0300

    Edit env variables names

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,48 +6,48 @@ const config = convict({
   db: {
     user: {
       doc: "DB user",
-      env: "ASTEA_INTERN_DB_USER",
+      env: "DB_USER",
       default: "postgres",
     },
     password: {
       doc: "DB password",
-      env: "ASTEA_INTERN_DB_PASS",
+      env: "DB_PASS",
       default: "postgres",
     },
     host: {
-      env: "ASTEA_INTERN_DB_HOST",
+      env: "DB_HOST",
       default: "localhost",
     },
     port: {
-      env: "ASTEA_INTERN_DB_PORT",
+      env: "DB_PORT",
       format: "port",
       default: 1234,
     },
     database: {
       doc: "DB name",
-      env: "ASTEA_INTERN_DB_NAME",
+      env: "DB_NAME",
       default: "postgres",
     },
     testDatabase: {
       doc: "DB name for testing",
-      env: "ASTEA_INTERN_TEST_DB_NAME",
+      env: "TEST_DB_NAME",
       default: "movies_project",
     },
   },
   port: {
-    env: "ASTEA_INTERN_SERVER_PORT",
+    env: "SERVER_PORT",
     format: "port",
     default: 3000,
   },
   jwt: {
     privateKey: {
       doc: "JWT private key",
-      env: "ASTEA_INTERN_JWT_PRIVATE_KEY",
+      env: "JWT_PRIVATE_KEY",
       default: "movies_private_key",
     },
     expiryTime: {
       doc: "JWT key expiry time",
-      env: "ASTEA_INTERN_JWT_EXPIRY_TIME",
+      env: "JWT_EXPIRY_TIME",
       default: "1h",
     },
   },
